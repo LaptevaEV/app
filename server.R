a3ac025c4660e1f874623ccdef0953c3d91a8805
Екатерина Лаптева < >
2018-06-18 23:09:37 +0500

    Формирование основы приложения

--- server.R
+++ server.R
@@ -1,3 +1,14 @@
 server <- function(input, output) {
-  
+  source("global_data.R")
+
+  ROOM=function(){input$room}
+  KM=function(){input$km}
+  FLOOR=function(){input$floor}
+  TOWN=function(){c(input$town)}
+
+  output$plot <- renderPlot({
+    #dataforplot=data[data$town==TOWN(),]
+    #dataforplot=aggregate(dataforplot,by=date, FUN=mean)
+    plot(x=data$date,y=data$resale_price, type="l")
+  })
 }
